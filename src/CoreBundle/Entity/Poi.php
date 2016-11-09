6f25f54c9c1fb90109fbac026a8b4e7de5a17d8b
ngdo-pro <ngdo27@gmail.com>
2016-11-09 14:00:22 +0100

    added longitude and latitude to POI

--- src/CoreBundle/Entity/Poi.php
+++ src/CoreBundle/Entity/Poi.php
@@ -90,6 +90,16 @@ class Poi
      */
     private $lastUpdate;
 
+    /**
+     * @var float
+     */
+    private $longitude;
+
+    /**
+     * @var float
+     */
+    private $latitude;
+
 
     /**
      * Get id
@@ -444,5 +454,51 @@ class Poi
     public function getLastUpdate()
     {
         return $this->lastUpdate;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     * @return Poi
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float 
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param float $latitude
+     * @return Poi
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return float 
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
     }
 }
