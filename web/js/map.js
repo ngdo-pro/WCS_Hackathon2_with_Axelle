a2d0116a60c1c6fbafb99b7ae5d417f6414d4460
ngdo-pro <ngdo27@gmail.com>
2016-11-10 02:24:01 +0100

    map.js display correct info

--- web/js/map.js
+++ web/js/map.js
@@ -6,40 +6,41 @@ function initMap() {
     xhr.onreadystatechange = function(){
         if(xhr.readyState == 4 && xhr.status == 200){
             var poi = JSON.parse(xhr.responseText);
-            alert(poi);
+            var coord = {lat: -34.397 , lng:  150.644};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: coord,
+                zoom: 18
+            });
+            var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">' + poi.name + '</h1>'+
+                '<div id="bodyContent">'+
+                '<p>' + poi.typeDetail + '</p>'+
+                '<p>' + poi.adress + '</p>'+
+                '<p>' + poi.postalCode + '</p>'+
+                '<p>' + poi.city + '</p>'+
+                '<p>' + poi.phoneNumber + '</p>'+
+                '<p>' + poi.openingHours + '</p>'+
+                '<p>' + poi.tariff + '</p>'+
+                '</div>'+
+                '</div>';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            var marker = new google.maps.Marker({
+                position: coord,
+                map: map,
+                title: poi.name
+            });
+            marker.addListener('click', function() {
+                infowindow.open(map, marker);
+            });
         }
     };
     xhr.send('variable=valeur');
-    var poi = {lat: -34.397 , lng:  150.644};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: poi,
-        zoom: 18
-    });
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading"></h1>'+
-        '<div id="bodyContent">'+
-        '<p>{{ poi.typeDetail }}</p>'+
-        '<p>{{ poi.adress }}</p>'+
-        '<p>{{ poi.postalCode }}</p>'+
-        '<p>{{ poi.city }}</p>'+
-        '<p>{{ poi.openingHours }}</p>'+
-        '<p>{{ poi.tariff }}</p>'+
-        '</div>'+
-        '</div>';
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    var marker = new google.maps.Marker({
-        position: poi,
-        map: map,
-        title: 'Uluru (Ayers Rock)'
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
 
 }
